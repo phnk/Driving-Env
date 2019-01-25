39c2ee7d610a630b18d15237e00b0191598f0066
Gerard Maggiolino <gerardmaggiolino@gmail.com>
2019-01-25 14:28:54 -0800

    Added debugging environment.

---
+++ gym_driving/resources/_debug_env.py
@@ -0,0 +1,28 @@
+import numpy as np
+import pybullet as p
+from gym_driving.resources import getResourcePath
+import gym_driving.resources._helper_functions as helper
+import gym_driving.resources._car as car
+import time
+
+
+
+def main(): 
+    p.connect(p.GUI)
+    p.setGravity(0, 0, -10)
+    t = p.addUserDebugParameter('Throttle', 0, 1, 0)
+    s = p.addUserDebugParameter('Steering Position', -.6, .6, 0)
+    c = car.Car()
+    p.loadURDF(getResourcePath('plane/plane.urdf'))
+    while True: 
+        throttle = p.readUserDebugParameter(t)
+        steer = p.readUserDebugParameter(s)
+        c.apply_action([throttle, steer])
+        p.stepSimulation()
+        time.sleep(0.004) 
+
+    p.disconnect()
+
+
+if __name__ == '__main__': 
+    main()
