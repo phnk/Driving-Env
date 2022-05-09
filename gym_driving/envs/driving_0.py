868c9efc31c6c75b3bfc2078e6dd9def1f49033f
carbor @ dgx <ph@dgx>
2022-05-09 11:23:18 +0000

    all except target vector

--- gym_driving/envs/driving_0.py
+++ gym_driving/envs/driving_0.py
@@ -47,14 +47,12 @@ class Driving0(DrivingEnv):
             dtype=np.float32
         )
         """
-        """
         self.orientation: gym.spaces.box.Box = gym.spaces.box.Box(
             low = -1,
             high = 1,
             shape = (2,),
             dtype=np.float32
         )
-        """
         
         self.velocity: gym.spaces.box.Box = gym.spaces.box.Box(
             low = -5,
@@ -72,7 +70,7 @@ class Driving0(DrivingEnv):
 
         self.observation_space = gym.spaces.dict.Dict({
 #                "position": self.position,
-#                "orientation": self.orientation,
+                "orientation": self.orientation,
                 "velocity": self.velocity,
                 "camera_image": self.camera_image
         })
@@ -150,7 +148,7 @@ class Driving0(DrivingEnv):
         camera_image = self.car.get_camera_image()
         return {
 #                "position": self.target - pos,
-#                "orientation": ori, 
+                "orientation": ori, 
                 "velocity": vel,
                 "camera_image": camera_image
                 }
